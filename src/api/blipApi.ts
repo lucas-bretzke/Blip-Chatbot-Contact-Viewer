68d2d51d0eb12c6372a85410bbb249e67a10cbcd
Lucas Bretzke <lucasbretzke@gmail.com>
2024-10-21 23:23:18 -0300

    feat: add API functions for login, get contacts, and get messages

--- src/api/blipApi.ts
+++ src/api/blipApi.ts
@@ -1,11 +1,8 @@
-// src/api/blipApi.ts
 import axios from 'axios'
 
-const API_BASE_URL = 'https://your-blip-api-url.com' // Substitua pela URL da API do Blip
+const API_BASE_URL = 'https://lucas-bretzke-fjdxg.http.msging.net/commands'
 
 export const login = async (apiKey: string) => {
-  // Implementar lógica para validar a chave da API
-  // Exemplo:
   const response = await axios.get(`${API_BASE_URL}/login`, {
     headers: {
       Authorization: `Bearer ${apiKey}`
@@ -19,26 +16,48 @@ export const getContacts = async (
   page: number,
   limit: number
 ) => {
-  const response = await axios.get(`${API_BASE_URL}/contacts`, {
-    headers: {
-      Authorization: `Bearer ${apiKey}`
+  const response = await axios.post(
+    API_BASE_URL,
+    {
+      id: Math.random().toString(36).substring(7),
+      to: 'postmaster@msging.net',
+      method: 'get',
+      uri: `/contacts?$skip=${(page - 1) * limit}&$take=${limit}`
     },
-    params: {
-      page,
-      limit
+    {
+      headers: {
+        Authorization: `key bWV1cHJpbWVpcm9jaGF0Ym90MzM6WWk2WXhFUGJtNzBJREVXcDNnaHM=`,
+        'Content-Type': 'application/json'
+      }
     }
-  })
+  )
+
+  const contatos = response.data.resource.items
+  console.log('Data', response.data.resource.items)
+
   return response.data
 }
 
 export const getMessages = async (apiKey: string, contactId: string) => {
-  const response = await axios.get(
-    `${API_BASE_URL}/contacts/${contactId}/messages`,
-    {
-      headers: {
-        Authorization: `Bearer ${apiKey}`
-      }
+  try {
+    const requestBody = {
+      id: Math.random().toString(36).substring(7),
+      to: 'postmaster@msging.net',
+      method: 'get',
+      uri: `/threads/${contactId}`
     }
-  )
-  return response.data
+
+    const response = await axios.post(API_BASE_URL, requestBody, {
+      headers: {
+        // Authorization: `key ${apiKey}`,
+        Authorization: `${apiKey}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    console.log('res: ', response.data.resource.items)
+    return response.data 
+  } catch (error) {
+    console.error('Erro ao buscar as mensagens: ', error)
+    throw error
+  }
 }
